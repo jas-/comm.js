70cb1a5b2d4641589aa7c4dacee9fea0aa08bca1
jas- <jason.gerfen@gmail.com>
2013-04-04 08:26:10 -0600

    Thu Apr  4 08:26:09 MDT 2013 - Resolved bug regarding error callback from _comm.ajax()

--- comm.js
+++ comm.js
@@ -347,7 +347,7 @@
 
 					error: function(xhr, status, error){
 						_log.error(o.appID, '_comm.ajax: '+status+' => '+error.message);
-						((o.errcallback)&&($.isFunction(o.errcallback))) ? o.errcallback.call(x) : false;
+						((o.errcallback)&&($.isFunction(o.errcallback))) ? o.errcallback.call(xhr) : false;
 					}
 				});
 				return _r;
