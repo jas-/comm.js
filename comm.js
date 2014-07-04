fc8900c15819262d0a4cf8c57c8bf88f59b85bdf
jas- <jason.gerfen@gmail.com>
2014-07-04 07:50:10 -0600

    Fix: Added lib.parse() function to handle JSON specific request types

--- comm.js
+++ comm.js
@@ -204,14 +204,22 @@
         }
 
         function headers() {
+					if (/put|post/.test(obj.method))
+						xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
           if (!reg.test(obj.url))
             xhr.withCredentials = true;
+
+					if (libs.parse(obj.data)) {
+						xhr.setRequestHeader('Content-Type', 'application/json');
+						obj.data = JSON.stringify(obj.data);
+					}
         }
 
         xhr.onreadystatechange = handler;
         xhr.open(obj.method, obj.url, obj.async);
         headers();
-        xhr.send(/post|put/i.test(obj.method) ? obj.data : null);
+        xhr.send(obj.data);
       }
     };
 
@@ -232,7 +240,7 @@
        *
        * @returns {Object}
        */
-      merge: function (defaults, obj) {
+      merge: function(defaults, obj) {
         defaults = defaults || {};
 
         for (var item in defaults) {
@@ -243,11 +251,29 @@
           obj[item] = defaults[item];
         }
 
-        obj.logID = obj.appID;
-
         return obj;
-      }
-    };
+      },
+
+
+      /**
+       * @function parse
+       * @scope private
+       * @abstract Attempt to parse a JSON object
+       *
+       * @param {String} str String to test
+       *
+       * @returns {Boolean}
+       */
+			parse: function(str) {
+				var ret = false;
+				try {
+					(/object/.test(str)) ? JSON.parse(str) : JSON.stringify(str);
+				} catch (e) {
+					ret = true;
+				}
+				return ret;
+			}
+		};
 
     /**
      * @function init
